19ba0f3a9b988cf986510fdfcfc8afc5cc022503
Dustin Serio <104038407+GigabitDut@users.noreply.github.com>
2022-06-30 21:28:59 -0500

    Add files via upload
    this code is not my own, this code is copied from a tutorial i did on youtube, used for educational purposes only

---
+++ JS Clock/app.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const mintueHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(mintueHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock();
