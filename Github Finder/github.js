fa861b2a41dde6ea5d4cefea13518ad1bfc2b07d
Dustin Serio <104038407+GigabitDut@users.noreply.github.com>
2022-06-30 21:35:39 -0500

    Add files via upload

---
+++ Github Finder/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '5c6b1134bd44e7401844';
+    this.client_secret = '9c55bd15d786c41a1e10e1241eebf8e77094cf59';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
